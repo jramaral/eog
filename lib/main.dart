57915ff4cdec844fde118703d2cc72b97ca01383
Jose Roberto <j.robertoamaral@gmail.com>
2023-04-03 11:07:50 -0400

    correção

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:eog/pages/home.page.dart';
-import 'package:eog/widgets/loading-button.widget.dart';
-import 'package:eog/widgets/logo.widget.dart';
-import 'package:eog/widgets/submit-form.dart';
-import 'package:eog/widgets/success.widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 
-import 'widgets/input.widget.dart';
 
 void main() => runApp(App());
 
